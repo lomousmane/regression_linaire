045023dce433269b6585d642ae2c5c7e969f8e0d
ousmane lom <lomousmane@gmail.com>
2024-06-26 23:14:16 +0000

    version_analyse_oats

---
+++ oats_data_analysis.R
@@ -0,0 +1,38 @@
+library(dplyr)
+library(tidyverse)
+data<-oats
+View(data)
+names(data)=c("block","variete","nitrogene","yield")
+str(data)
+summary(data)
+# analyse univarié
+table(data$variete)
+# visualisation du rendement
+ggplot(data,aes(x=yield),color="blue")+geom_histogram(bins=10)
+
+# analyse bivariee
+p=ggplot(data,aes(x=variete,y=yield,fill=variete))+
+geom_boxplot()+theme_bw()
+p
+ggplotly(p)
+
+p_1=ggplot(data,aes(x=block,y=yield,fill=variete))+
+    geom_boxplot()+theme_bw()
+p_1
+ggplotly(p)
+# relation nirogen et variété
+
+p_2=ggplot(data,aes(x=nitrogene,y=yield,fill=variete))+
+    geom_boxplot()+theme_bw()
+p_2
+ggplotly(p)
+
+# analyse de la regression linéaire
+modele=lm(yield~nitrogene+variete,data=data)
+modele
+summary(modele)
+# analyse de la variance
+anova(modele)
+
+
+
