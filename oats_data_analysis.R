6cf82c24dd8de32eff3353ea71097cbc377f0487
lomousmane <lomousmane@gmail.com>
2024-06-27 00:28:42 +0000

    Update oats_data_analysis.R

--- oats_data_analysis.R
+++ oats_data_analysis.R
@@ -1,21 +1,23 @@
 library(dplyr)
 library(tidyverse)
 data<-oats
+#oberver les données
 View(data)
 names(data)=c("block","variete","nitrogene","yield")
 str(data)
+#resumé des données
 summary(data)
 # analyse univarié
 table(data$variete)
-# visualisation du rendement
+# visualisation du rendement histogramme
 ggplot(data,aes(x=yield),color="blue")+geom_histogram(bins=10)
 
-# analyse bivariee
+# analyse bivariee : variete et rendement
 p=ggplot(data,aes(x=variete,y=yield,fill=variete))+
 geom_boxplot()+theme_bw()
 p
 ggplotly(p)
-
+# relation entre block et rendement
 p_1=ggplot(data,aes(x=block,y=yield,fill=variete))+
     geom_boxplot()+theme_bw()
 p_1
